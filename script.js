e01e41c4b18fe44c722269ce648b41314d8a2161
Tyler Hawkins <thawkin3@byu.net>
2018-08-02 00:18:22 -0600

    bricks are added and character can jump; haven't figured out hit detection yet

--- script.js
+++ script.js
@@ -1,3 +1,55 @@
 $(document).ready(function() {
-	console.log('ready!');
+	// globals
+	var canJump = false;
+	var gameIsInProgress = false;
+	var brickCreatorInterval;
+	var score = 0;
+	var highscore = 0;
+
+	// start the game
+	$('#startButton').on('click', function() {
+		$('#startButton, #instructions').hide();
+		$('h2').css('visibility', 'hidden');
+		$('.catContainer').removeClass('homeScreen');
+		$('.catContainer, .brickContainer').animate({
+			top: 40,
+		}, function() {
+			canJump = true;
+			gameIsInProgress = true;
+			if (typeof brickCreatorInterval === 'undefined') {
+				brickCreatorInterval = setInterval(function() {
+					sendBrick();
+				}, 2500);
+			}
+		});
+	});
+
+	// jump
+	$(document).on('keyup', function() {
+		if (canJump) {
+			canJump = false;
+			$('.catContainer').addClass('jump');
+			setTimeout(function() {
+				$('.catContainer').removeClass('jump');
+				canJump = true;
+			}, 700);
+		}
+	});
+
+	function sendBrick() {
+		// move the stack down
+		$('.brick').animate({ top: '+=20' });
+		// $('.catContainer, .brick').animate({ top: '+=20' });
+
+		// create a new brick
+		var startingSide = Math.random() > 0.5 ? '-' : '';
+		var speed = Math.floor(Math.random() * 500) + 1500;
+		$('.brickContainer').prepend('<div class="brick" style="left: ' + startingSide + '1000px;"></div>');
+		$('.brick').eq(0).animate({
+			left: '50%',
+		}, speed, function() {
+			score++;
+			$('#score #value').text(score);
+		});
+	}
 });
