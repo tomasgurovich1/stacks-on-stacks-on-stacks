ca8ee23124e88f578063c024b66fd17a472cf9aa
Tyler Hawkins <thawkin3@byu.net>
2018-08-27 23:18:29 -0600

    game play is working nicely now; no restart button yet

--- script.js
+++ script.js
@@ -2,54 +2,118 @@ $(document).ready(function() {
 	// globals
 	var canJump = false;
 	var gameIsInProgress = false;
+	var catJumpedOverCurrentBlock = false;
+	var catIsOnHisWayDown = false;
 	var brickCreatorInterval;
 	var score = 0;
 	var highscore = 0;
 
 	// start the game
 	$('#startButton').on('click', function() {
+		$('#startButton').off('click');
 		$('#startButton, #instructions').hide();
 		$('h2').css('visibility', 'hidden');
 		$('.catContainer').removeClass('homeScreen');
-		$('.catContainer, .brickContainer').animate({
+		$('.catContainerOuter, .brickContainer').animate({
 			top: 40,
 		}, function() {
 			canJump = true;
 			gameIsInProgress = true;
+			mainLoop();
 			if (typeof brickCreatorInterval === 'undefined') {
 				brickCreatorInterval = setInterval(function() {
 					sendBrick();
-				}, 2500);
+				}, 4000);
 			}
 		});
 	});
 
 	// jump
 	$(document).on('keyup', function() {
+		if (!gameIsInProgress) {
+			$('#startButton').click();
+		}
 		if (canJump) {
 			canJump = false;
-			$('.catContainer').addClass('jump');
-			setTimeout(function() {
-				$('.catContainer').removeClass('jump');
-				canJump = true;
-			}, 700);
+			$('.catStanding').css('opacity', 0);
+			$('.catJumping').css('opacity', 1);
+			$('.catContainer').animate({ top: '-=80px' }, 300, function() {
+				catIsOnHisWayDown = true;
+				$('.catContainer').animate({ top: '+=80px' }, 300, function() {
+					canJump = true;
+					catIsOnHisWayDown = false;
+					$('.catStanding').css('opacity', 1);
+					$('.catJumping').css('opacity', 0);
+				});
+			});
 		}
 	});
 
+	// send a new brick from the left or right of the screen and move the stack down
 	function sendBrick() {
 		// move the stack down
-		$('.brick').animate({ top: '+=20' });
-		// $('.catContainer, .brick').animate({ top: '+=20' });
+		$('.catContainerOuter, .brick').animate({ top: '+=20' });
 
 		// create a new brick
 		var startingSide = Math.random() > 0.5 ? '-' : '';
-		var speed = Math.floor(Math.random() * 500) + 1500;
-		$('.brickContainer').prepend('<div class="brick" style="left: ' + startingSide + '1000px;"></div>');
+		var speed = Math.floor(Math.random() * 500) + 3000;
+		$('.brickContainer').prepend('<div class="brick" style="left: ' + startingSide + '700px;"></div>');
 		$('.brick').eq(0).animate({
 			left: '50%',
 		}, speed, function() {
 			score++;
 			$('#score #value').text(score);
+			catJumpedOverCurrentBlock = false;
 		});
 	}
+
+	// check if you've been hit by a brick
+	function hitCheck() {
+		var $bricks = $('.brick');
+		var brickLeftPosition = parseInt($bricks.eq(0).css('left'));
+		var $catContainer = $('.catContainer').eq(0);
+		var $catContainerOuter = $('.catContainerOuter').eq(0);
+		
+		// cat was hit
+		if (!catJumpedOverCurrentBlock && $bricks.length > 1 && ((brickLeftPosition <= 90 && brickLeftPosition >= 75) || (brickLeftPosition <= 205 && brickLeftPosition >= 190)) && parseInt($catContainer.css('top')) > -20) {
+			console.warn('game over!');
+			console.log('brick left: ' + $('.brick').eq(0).css('left'));
+			console.log('catContainer top: ' + $('.catContainer').eq(0).css('top'));
+			console.log('catContainerOuter top: ' + $('.catContainerOuter').eq(0).css('top'));
+			$bricks.eq(0).stop(true, false);
+			$catContainer.stop(true, false);
+			canJump = false;
+			gameIsInProgress = false;
+			clearInterval(brickCreatorInterval);
+
+			var brickWasComingFromLeft = brickLeftPosition <= 90 && brickLeftPosition >= 75;
+			brickWasComingFromLeft ? $catContainer.addClass('dead right') : $catContainer.addClass('dead left')
+		}
+
+		// cat is on top of the brick
+		if ($catContainerOuter.css('top') === '60px' && !catJumpedOverCurrentBlock && $bricks.length > 1 && brickLeftPosition >= 75 && brickLeftPosition <= 205 && parseInt($catContainer.css('top')) <= -20 && parseInt($catContainer.css('top')) >= -30 && catIsOnHisWayDown) {
+			console.warn('on top of brick!');
+			console.log('brick left: ' + $('.brick').eq(0).css('left'));
+			console.log('catContainer top: ' + $('.catContainer').eq(0).css('top'));
+			console.log('catContainerOuter top: ' + $('.catContainerOuter').eq(0).css('top'));
+			$catContainer.stop(false, false);
+			$catContainer.css('top', 0);
+			$catContainerOuter.css('top', '-=20');
+			canJump = true;
+			catJumpedOverCurrentBlock = true;
+			$('.catStanding').css('opacity', 1);
+			$('.catJumping').css('opacity', 0);
+		}
+	}
+
+	// main loop
+	function mainLoop() {			
+			if (gameIsInProgress) {
+				hitCheck();
+				window.requestAnimationFrame(mainLoop);
+			} else {
+				// submit your score
+
+			}
+		};
 });
